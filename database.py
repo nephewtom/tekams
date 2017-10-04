86edd4d374a2b468c125484703cbb282cded4160
Tomas Orti <tomas.orti.martin@ericsson.com>
2017-10-04 04:52:30 +0200

    Catch DB exceptions

--- database.py
+++ database.py
@@ -25,6 +25,7 @@ class Database:
 
     def __insert(self, uid, page, ranking, name, address, city, province,
                  phone, cnae, billing, employees, status):
+
         self.cursor.execute(
             "INSERT INTO company(uid, page, ranking, name, address, city, \
             province, phone, cnae, billing, employees, status) "
@@ -33,21 +34,42 @@ class Database:
              province, phone, cnae, billing, employees, status])
         self.connection.commit()
 
-    def insert(self, uid, record, status):
-        self.__insert(uid, record.page, record.ranking, record.name,
-                      record.address, record.city, record.province,
-                      record.phone, record.cnae, record.billing,
-                      record.employees, status)
-
-    def captcha(self, uid, page, success):
+    def __captcha(self, uid, page, success):
         success_int = 1 if success else 0
         self.cursor.execute(
             "INSERT INTO captcha(uid, page, success, datetime) \
             VALUES(?,?,?,?)", [uid, page, success_int, datetime.now()])
         self.connection.commit()
 
-    def conns(self, uid, query, ip, url):
+    def __conns(self, uid, query, ip, url):
         self.cursor.execute(
             "INSERT INTO conns(uid, query, ip, url, datetime) \
             VALUES(?,?,?,?,?)", [uid, query, ip, url, datetime.now()])
         self.connection.commit()
+
+    def insert(self, uid, record, status):
+        try:
+            self.__insert(uid, record.page, record.ranking, record.name,
+                          record.address, record.city, record.province,
+                          record.phone, record.cnae, record.billing,
+                          record.employees, status)
+        except:
+            print("EXCEPTION db.insert BEGIN")
+            record.display()
+            print("EXCEPTION db.insert END")
+
+    def captcha(self, uid, page, success):
+        try:
+            self.__captcha(uid, page, success)
+        except:
+            print("EXCEPTION db.captcha BEGIN")
+            print("uid:", uid, "| page:", page)
+            print("EXCEPTION db.captcha END")
+
+    def conns(self, uid, query, ip, url):
+        try:
+            self.__conns(uid, query, ip, url)
+        except:
+            print("EXCEPTION db.conns BEGIN")
+            print("uid:", uid, "| query:", query, "url:", url)
+            print("EXCEPTION db.conns END")
